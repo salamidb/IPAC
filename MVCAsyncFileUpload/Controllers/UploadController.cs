a9a7d53198b559de6578bfe3abfdff22e478eacb
salamidb <a.salamidb@gmail.com>
2014-11-17 17:31:03 +0600

    17-11-2014 MVC file Upload

---
+++ MVCAsyncFileUpload/Controllers/UploadController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCAsyncFileUpload.Controllers
+{
+    public class UploadController : Controller
+    {
+        //
+        // GET: /Upload/
+
+        public ActionResult AsyncUpload()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AsyncUpload(IEnumerable<HttpPostedFileBase> files)
+        {
+            int count = 0;
+            if (files != null)
+            {
+                foreach (var file in files)
+                {
+                    if (file != null && file.ContentLength > 0)
+                    {
+                        var fileName = Guid.NewGuid() + Path.GetExtension(file.FileName);
+                        var path = Path.Combine(Server.MapPath("~/UploadedFiles"), fileName);
+                        file.SaveAs(path);
+                        count++;
+                    }
+                }
+            }
+            return new JsonResult { Data = "Successfully " + count+ " file(s) uploaded" };
+        }
+
+    }
+
+    
+}
